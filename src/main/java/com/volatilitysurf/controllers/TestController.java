d6fa40fbfc98944a256566e5418eccc03fde719a
unclebabykern <104476731+unclebabykern@users.noreply.github.com>
2022-08-25 09:34:55 -0600

    Format negative change on options header

--- src/main/java/com/volatilitysurf/controllers/TestController.java
+++ src/main/java/com/volatilitysurf/controllers/TestController.java
@@ -2,6 +2,7 @@ package com.volatilitysurf.controllers;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 
 import javax.servlet.http.HttpSession;
 
@@ -16,42 +17,37 @@ import com.volatilitysurf.models.Stock;
 
 public class TestController {
 	
-	public void testTwelve(HttpSession session) throws UnsupportedEncodingException {
+	public void retrieveTwelveDates(HttpSession session) throws UnsupportedEncodingException {
 		Stock ticker = (Stock) session.getAttribute("ticker");
 		String symbol = ticker.getSymbol();
 		String host1 = "https://twelve-data1.p.rapidapi.com/options/expiration?";
-		String host2 = "https://twelve-data1.p.rapidapi.com/options/";
 		String charset = "UTF-8";
 		String x_rapidapi_key = "ec2d471e81mshd1d781daa45de8ap15487djsn98d072fb2757";
-		String x_rapidapi_host = "mboum-finance.p.rapidapi.com";//Change
+		String x_rapidapi_host = "twelve-data1.p.rapidapi.com";
 		String query1 = String.format("symbol=%s", URLEncoder.encode(symbol, charset));
-		String query2 = String.format("symbol=%s", URLEncoder.encode(symbol, charset));
 		String url1 = host1 + query1;//Change??
-		String url2 = host2 + "?" + query2;
-		//CALL TO TWELVE API USING TICKER SYMBOL TO GATHER ~11 OPTION EXPIRATION DATES
-		HttpResponse<JsonNode> response;
-		try { response = Unirest.get(url1)
+		//CALL TO TWELVE API USING TICKER SYMBOL TO GATHER OPTION EXPIRATION DATES
+		HttpResponse<JsonNode> response1;
+		try { response1 = Unirest.get(url1)
 				.header("X-RapidAPI-Key", x_rapidapi_key)
 				.header("X-RapidAPI-Host", x_rapidapi_host)
 				.asJson();
-			String status = response.getBody().getObject().getString("status");
+			String status = response1.getBody().getObject().getString("status");
 			if(status == "error") {
 				System.out.println("Error getting request...");
 				return;
 			}
-			JSONArray result = response.getBody().getObject().getJSONArray("dates");
-//			for(each : result) {
-//				
-//			}
+			JSONArray result = response1.getBody().getObject().getJSONArray("dates");
+			ArrayList<String> dates = new ArrayList<String>();
+			for(int i = 0; i < result.length();i++) {
+				
+				
+			}
 		} catch (UnirestException e) {
 			System.out.printf("get: %s", e.getMessage());
 		}
 		
-		//CALL TWELVE AGAIN TO GET STOCKS FROM SUBSEQUENT GATHERED EXPIRIES
-//		HttpResponse<String> response = Unirest.get("https://twelve-data1.p.rapidapi.com/options/chain?symbol=AAPL&expiration_date=2022-08-26")
-//				.header("X-RapidAPI-Key", "ec2d471e81mshd1d781daa45de8ap15487djsn98d072fb2757")
-//				.header("X-RapidAPI-Host", "twelve-data1.p.rapidapi.com")
-//				.asString();
+		
 		
 	}
 }
