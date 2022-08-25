0bd7d552f7c134f42e6ac1cfbcf7c9ae4c0fe70a
Erik van Erp <jhamvanerp@gmail.com>
2022-08-24 22:15:40 -0400

    validations for all API fields added to Stock and Option service

--- src/main/java/com/volatilitysurf/services/StockService.java
+++ src/main/java/com/volatilitysurf/services/StockService.java
@@ -18,16 +18,53 @@ public class StockService {
 		Stock stock = new Stock();
 		
 		stock.setSymbol(quote.getString("symbol"));
-		stock.setShortName(quote.getString("shortName"));
-		stock.setQuoteSourceName(quote.getString("quoteSourceName"));
-		stock.setFullExchangeName(quote.getString("fullExchangeName"));
-		stock.setCurrency(quote.getString("currency"));
-		stock.setRegularMarketPrice(quote.getDouble("regularMarketPrice"));
-		stock.setRegularMarketChange(quote.getDouble("regularMarketChange"));
-		stock.setRegularMarketChangePercent(quote.getDouble("regularMarketChangePercent"));
-		Timestamp rmt = new Timestamp(quote.getLong("regularMarketTime") * 1000);
-		stock.setRegularMarketTime(rmt);
-		stock.setExchangeTimezoneShortName(quote.getString("exchangeTimezoneShortName"));
+		
+		if(quote.has("shortName")) {
+			stock.setShortName(quote.getString("shortName"));			
+		} else {
+			stock.setShortName(null);
+		}
+		if(quote.has("quoteSourceName")) {
+			stock.setQuoteSourceName(quote.getString("quoteSourceName"));			
+		} else {
+			stock.setQuoteSourceName(null);
+		}
+		if(quote.has("fullExchangeName")){			
+			stock.setFullExchangeName(quote.getString("fullExchangeName"));
+		} else {
+			stock.setFullExchangeName(null);
+		}
+		if(quote.has("currency")) {
+			stock.setCurrency(quote.getString("currency"));			
+		} else {
+			stock.setCurrency(null);
+		}
+		if(quote.has("regularMarketPrice")){			
+			stock.setRegularMarketPrice(quote.getDouble("regularMarketPrice"));
+		} else {
+			stock.setRegularMarketPrice(null);
+		}
+		if(quote.has("regularMarketChange")) {
+			stock.setRegularMarketChange(quote.getDouble("regularMarketChange"));			
+		} else {
+			stock.setRegularMarketChange(null);
+		}
+		if(quote.has("regularMarketChangePercent")) {
+			stock.setRegularMarketChangePercent(quote.getDouble("regularMarketChangePercent"));			
+		} else {
+			stock.setRegularMarketChangePercent(null);
+		}
+		if(quote.has("regularMarketTime")) {
+			Timestamp rmt = new Timestamp(quote.getLong("regularMarketTime") * 1000);
+			stock.setRegularMarketTime(rmt);			
+		} else {
+			stock.setRegularMarketTime(null);
+		}
+		if(quote.has("exchangeTimezoneShortName")) {
+			stock.setExchangeTimezoneShortName(quote.getString("exchangeTimezoneShortName"));			
+		} else {
+			stock.setExchangeTimezoneShortName(null);
+		}
 		
 		Stock newStock = stockRepo.save(stock);
 		return newStock;
